b277c95d70167a26cef937271f05f6ac130ae8ea
taccumei <80431063+taccumei@users.noreply.github.com>
2022-02-09 09:54:16 -0800

    BookaLayout
    BookaLayout for project proposal

---
+++ BookaApp/ProjectLayout/app/src/main/java/com/example/projectlayout/BorrowRequestActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BorrowRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_borrow_request);
+    }
+}

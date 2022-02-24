1c13a53da99c14c338acebc378643244de4c114d
Minh Bui <bqminh1806@gmail.com>
2022-02-24 09:01:24 -0800

    Remove unused import

--- BookaApp/ProjectLayout/app/src/main/java/com/example/projectlayout/SplashActivity.java
+++ BookaApp/ProjectLayout/app/src/main/java/com/example/projectlayout/SplashActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import java.sql.Time;
 import java.util.Timer;
 import java.util.TimerTask;
 

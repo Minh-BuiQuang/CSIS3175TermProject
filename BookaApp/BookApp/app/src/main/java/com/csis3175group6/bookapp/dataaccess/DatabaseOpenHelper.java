85ef910b6d2112bbaf5866a7a170d063738cffa8
Minh Bui <bqminh1806@gmail.com>
2022-03-11 21:28:30 -0800

    Add database manipulation methods for books and users

--- BookaApp/BookApp/app/src/main/java/com/csis3175group6/bookapp/dataaccess/DatabaseOpenHelper.java
+++ BookaApp/BookApp/app/src/main/java/com/csis3175group6/bookapp/dataaccess/DatabaseOpenHelper.java
@@ -8,13 +8,12 @@ import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 import java.sql.SQLException;
+import java.util.List;
 
 public class DatabaseOpenHelper extends OrmLiteSqliteOpenHelper {
 
     final static String DATABASE_NAME = "BookManagement.db";
     final static int VERSION = 1;
-    private Dao<Book, Long> BookDao;
-    private Dao<User, Long> UserDao;
 
     public DatabaseOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, VERSION, R.raw.ormlite_config2);
@@ -43,5 +42,82 @@ public class DatabaseOpenHelper extends OrmLiteSqliteOpenHelper {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public boolean insertBook(Book book){
+        try {
+            Dao<Book, Long> bookDao = getDao(Book.class);
+            int affectedRows = bookDao.create(book);
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean updateBook(Book book) {
+
+        try {
+            Dao<Book, Long> bookDao = getDao(Book.class);
+            int affectedRows = bookDao.update(book);
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public List<Book> getBooksByOwnerId(Long ownerId){
+        try {
+            Dao<Book, Long> bookDao = getDao(Book.class);
+            List<Book> books = bookDao.queryBuilder().where().eq("ownerId", ownerId).query();
+            return books;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Book> getBooksByHolderId(Long holderId){
+        try {
+            Dao<Book, Long> bookDao = getDao(Book.class);
+            List<Book> books = bookDao.queryBuilder().where().eq("holderId", holderId).query();
+            return books;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<User> getUsers() {
+        try {
+            Dao<User, Long> userDao = getDao(User.class);
+            return userDao.queryForAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public User getUserByCredential(String name, String pinCode) {
+        try {
+            Dao<User, Long> userDao = getDao(User.class);
+            User user = userDao.queryBuilder().where().eq("Name", name).and().eq("pinCode", pinCode).queryForFirst();
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean updateUser(User user){
+        try {
+            Dao<User, Long> userDao = getDao(User.class);
+            int affectedRows = userDao.update(user);
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }

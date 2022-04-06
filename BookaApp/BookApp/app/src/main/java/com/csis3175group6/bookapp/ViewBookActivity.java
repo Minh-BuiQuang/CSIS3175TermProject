ee00445707bac4743c43cc3e99007089167cadb8
Minh Bui <bqminh1806@gmail.com>
2022-04-05 19:24:41 -0700

    Add returning book logic

--- BookaApp/BookApp/app/src/main/java/com/csis3175group6/bookapp/ViewBookActivity.java
+++ BookaApp/BookApp/app/src/main/java/com/csis3175group6/bookapp/ViewBookActivity.java
@@ -2,18 +2,23 @@ package com.csis3175group6.bookapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.csis3175group6.bookapp.dataaccess.DatabaseOpenHelper;
 import com.csis3175group6.bookapp.entities.Book;
+import com.csis3175group6.bookapp.entities.User;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 public class ViewBookActivity extends AppCompatActivity implements BookAdapter.ItemClickListener {
@@ -32,6 +37,10 @@ public class ViewBookActivity extends AppCompatActivity implements BookAdapter.I
     @Override
     protected void onResume() {
         super.onResume();
+        reloadBooks();
+    }
+
+    private void reloadBooks() {
         RecyclerView recyclerView = findViewById(R.id.book_recyclerview);
         DatabaseOpenHelper db = new DatabaseOpenHelper(this);
         //Get books and request counts for each book
@@ -46,6 +55,7 @@ public class ViewBookActivity extends AppCompatActivity implements BookAdapter.I
         adapter.setItemClickListener(this);
         recyclerView.setAdapter(adapter);
     }
+
     //Implement go back event for Back button on action bar
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -59,16 +69,47 @@ public class ViewBookActivity extends AppCompatActivity implements BookAdapter.I
 
     @Override
     public void onItemClick(View view, int position) {
+        Book book = books.get(position);
+
+        //If book is already lent away, show UI to take back the book
+        if(book.Status.equals(Book.STATUS_RENTED) || book.Status.equals(Book.STATUS_OVERDUE)) {
+            DatabaseOpenHelper db = new DatabaseOpenHelper(this);
+            User holder = db.getUser(book.HolderId);
+            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which){
+                        case DialogInterface.BUTTON_POSITIVE:
+                            book.HolderId = App.getInstance().User.Id;
+                            book.RentPrice = -1;
+                            book.RentedTime = new Timestamp(0);
+                            book.RentInformation = null;
+                            book.RentDuration = -1;
+                            book.Status = Book.STATUS_ACTIVE;
+                            db.updateBookRecord(book);
+                            Toast.makeText(ViewBookActivity.this, "The book has been returned to you!", Toast.LENGTH_LONG).show();
+                            reloadBooks();
+                            break;
+                        case DialogInterface.BUTTON_NEGATIVE:
+                            break;
+                    }
+                }
+            };
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("This book is being kept by " + holder.Name + "\nHas the person given it back?").setPositiveButton("Yes", dialogClickListener)
+                    .setNegativeButton("No", dialogClickListener).show();
+        }
         //Show list of requesters if this book has requests
-        if(requestCounts.get(position) > 0){
+        else if(requestCounts.get(position) > 0){
             Intent intent = new Intent(this, UserActivity.class);
-            intent.putExtra("bookId", books.get(position).Id);
+            intent.putExtra("bookId", book.Id);
             startActivity(intent);
         }
         //Allow user to create a new/update/cancel sharing this book
         else {
             Intent intent = new Intent(this, ShareBookActivity.class);
-            intent.putExtra("bookId", books.get(position).Id);
+            intent.putExtra("bookId", book.Id);
             startActivity(intent);
         }
     }
